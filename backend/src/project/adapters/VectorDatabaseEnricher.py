8fecf72a58496a78b25fda7a9865914dba85be22
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-09-15 17:05:35 -0300

    Refactor VectorData to instance vectorstore on init
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- backend/src/project/adapters/VectorDatabaseEnricher.py
+++ backend/src/project/adapters/VectorDatabaseEnricher.py
@@ -7,21 +7,20 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 class VectorDatabaseEnricher(ContextEnricher):
 
-    def getData(self, query):
+    def __init__(self):
         loader = TextLoader("src/project/database/textobase.txt")
         documento = loader.load()
 
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         splits = text_splitter.split_documents(documento)
-        vectorstore = Chroma.from_documents(
+        
+        self.vectorstore = Chroma.from_documents(
         documents=splits,
         embedding=OpenAIEmbeddings(),
         persist_directory="../database/chroma_db",
         collection_name='vector_database')
-
-        retriever = vectorstore.as_retriever()
-
-        return retriever
     
-
- 
+    def getData(self, query):
+        
+        retriever = self.vectorstore.as_retriever()
+        return retriever
