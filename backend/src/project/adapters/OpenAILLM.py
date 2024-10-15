1d30b070b3b0c2e7d2aaea88cbe550591fe14b48
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-10-15 14:19:02 -0300

    change the standart prompt to a customized one
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- backend/src/project/adapters/OpenAILLM.py
+++ backend/src/project/adapters/OpenAILLM.py
@@ -2,8 +2,9 @@ from project.ports.LLMAnswerer import LLMAnswerer
 from project.core.RagChain import RagChain
 
 from langchain_openai import ChatOpenAI
-from langchain import hub
 from dotenv import load_dotenv
+
+from langchain_core.prompts import PromptTemplate
 
 import os
 
@@ -16,9 +17,21 @@ class OpenAILLM(LLMAnswerer):
         os.environ["LANGCHAIN_API_KEY"] = settings.security.LANGCHAIN_API_KEY
 
         llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
-        prompt = hub.pull("rlm/rag-prompt")
 
-        rag_chain = RagChain(context, prompt, llm, query)
+        custom_template = """Use the following pieces of context to answer the question at the end.
+            If you don't know the answer, just say that you don't know, don't try to make up an answer.
+            Give the answer keeping in mind that you are a dungeon master assistant and need to follow the context as much as possible.
+            Always say "thanks for asking!" at the end of the answer.
+
+        {context}
+
+        Question: {question}
+
+        Helpful Answer:"""
+
+        custom_rag_prompt = PromptTemplate.from_template(custom_template)
+
+        rag_chain = RagChain(context, custom_rag_prompt, llm, query)
 
         answer = rag_chain.answer(query)
 
