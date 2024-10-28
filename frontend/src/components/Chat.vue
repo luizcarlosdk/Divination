98f82761bb4ef0ece9f4b5afed505793e559e3b5
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-10-28 19:39:52 -0300

    start to create chat
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    <div id="chat-view" >this is the space for the chat</div>
-  </div>
+  <v-container class="fill-height pa-0">
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>Chat Dungeons and Dragons</v-card-title>
+          <v-card-text class="flex-shrink-1">
+            <v-text-field
+              v-model="inputText"
+              label="Digite sua pergunta"
+              type="text"
+              no-details
+              outlined
+              append-outer-icon="send"
+              hide-details
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    inputText: '',
+  }),
 }
 </script>
 
 <style>
-
 </style>
