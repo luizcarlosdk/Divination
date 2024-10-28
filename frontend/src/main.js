b1ead1a1527bb6e78def921c4e234a0e9fbe69a0
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-10-28 19:15:53 -0300

    add vuetify
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,24 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+// Components
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
+app.use(vuetify)
 app.use(router)
-
 app.mount('#app')
