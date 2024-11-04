750b2b5dc334f7b9b33b178cd2a4e536d8007647
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-11-04 09:35:12 -0300

    start to implement new routes to handle new chat functionality
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- backend/src/project/adapters/AnswerRouter.py
+++ backend/src/project/adapters/AnswerRouter.py
@@ -9,8 +9,11 @@ from project.adapters.answer_dto import (
 
 
 class AnswerRouter(Router):
-    def __init__(self, chat_service: ChatService):
+    def __init__(
+        self, chat_service: ChatService, chat_repository: ChatRepository
+    ):
         self.chat_service = chat_service
+        self.chat_repository = chat_repository
 
     def create(self):
         router = APIRouter()
@@ -24,8 +27,28 @@ class AnswerRouter(Router):
 
         @router.post("/v1/answer")
         def get_answer(request: AnswerRequest) -> AnswerResponse:
-            answer = self.chat_service.get_answer(query=request.user_question)
-
+            answer = self.chat_service.get_answer(
+                query=request.user_question, chat_id=request.chat_id
+            )
             return AnswerResponse.create_answer(answer)
 
+        @router.post("/v1/chats")
+        def create_chat() -> CreateChatResponse:
+            new_chat_id = self.chat_repository.create()
+
+            return CreateChatResponse.create(new_chat_id)
+
+        @router.get("/v1/chats/:id")
+        def show_chat(id: str) -> ShowChatResponse:
+            history = self.chat_repository.get_history(id)
+            return ShowChatResponse.create(history)
+
+        @router.get("/v1/chats")
+        def list_chats() -> ListChatsResponse:
+            chats = self.chat_repository.get_all()
+
+            return ListChatsResponse.create(chats)
+
         return router
+
+        # user_question
--- backend/src/project/core/ChatService.py
+++ backend/src/project/core/ChatService.py
@@ -1,15 +1,31 @@
 class ChatService:
-    def __init__(self, context_enricher, answerer, template, settings):
+    def __init__(
+        self, context_enricher, answerer, template, chat_repository, settings
+    ):
         self.context_enricher = context_enricher
         self.llm_answerer = answerer
         self.answer_template = template
+        self.chat_repository = chat_repository
         self.project_settings = settings
 
-    def get_answer(self, query):
+    def get_answer(self, query, chat_id):
         context = self.context_enricher.getData(query)
+        chat = self.chat_repository.getHistory(chat_id, max=10)
         template = self.answer_template.getTemplate()
         answer = self.llm_answerer.getAnswer(
-            query, context, template, self.project_settings
+            query, context, chat, template, self.project_settings
         )
 
         return answer
+
+    # def create chat
+
+
+# na requisição vamos receber
+
+# id do usuário - pensar em como criar
+# personalidade do bot
+# pergunta
+# id da conversa
+#
+# chat service vai ser responsável por criar um novo chat
